1cea691ba1afc2a907213783d29f3d2c7e3bdd03
Jaimie Hart <hartjme@gmail.com>
2022-04-04 18:01:15 -0400

    changed back to localhost due to deployment issues

---
+++ src/components/routes/Artist/index.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import { parse, stringify } from "qs";
+
+import { getArtistReleases } from "/Users/jaimiehart/workspace/client-capstone/wax/src/API/getArtistReleases.js";
+
+import { Button } from "/Users/jaimiehart/workspace/client-capstone/wax/src/components/search/Button/index.js";
+import { ReleaseList } from "/Users/jaimiehart/workspace/client-capstone/wax/src/components/search/ReleaseList/index.js";
+
+import styles from "./artist.module.css";
+
+export class Artist extends Component {
+  constructor(props) {
+    super(props);
+
+    const { id } = parse(window.location.search, {
+      ignoreQueryPrefix: true
+    });
+
+    this.state = {
+      releaseId: id,
+      release: {}
+    };
+
+  }
+
+  async componentDidMount() {
+    const { releaseId } = this.state;
+    const releases = await getArtistReleases(releaseId);
+
+    console.log(releases);
+    this.setState({
+      release: releases
+    });
+  }
+
+  
+
+  goBack = () => {
+    window.history.back();
+  };
+
+  render() {
+    return (
+      <div className={styles.container}>
+        <div className={styles.controls}>
+          <Button onClick={this.goBack} highlight>
+            Go Back
+          </Button>
+          
+        </div>
+        <div className={styles.list}>
+          <ReleaseList release={this.state.release} />
+        </div>
+      </div>
+    );
+  }
+}
